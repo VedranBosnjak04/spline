fb49951aa17f32e78acb638aa11adf83b3c5d7de
VedranBosnjak04 <bosnjakvedran427@gmail.com>
2026-04-20 01:49:16 +0200

    Update Kubicni_spline.py

--- Kubicni_spline.py
+++ Kubicni_spline.py
@@ -102,56 +102,72 @@ if st.button("IZRAČUNAJ"):
         st.pyplot(fig)
 
         # --- DETALJAN RAČUN ---
-                # --- DETALJAN RAČUN ---
-        st.markdown("<h3 style='text-align: center;'>DETALJAN RAČUN</h3>", unsafe_allow_html=True)
+                # --- DETALJAN RAČUN (LaTeX stil) ---
+        st.divider()
+        st.latex(r"\text{\huge RAČUN}")
         
         c1, c2 = st.columns(2)
         
         with c1:
-            # Otvaramo div za lijevo poravnanje
             st.markdown('<div style="text-align: left;">', unsafe_allow_html=True)
             
-            # Grupiramo podatke
-            podatci_latex = r"\textbf{Podatci:}\\" + r"\begin{aligned}"
+            # Podatci
+            p_lat = r"\textbf{Podatci:}\\" + r"\begin{aligned}"
             for i, p_val in enumerate(podatci):
-                podatci_latex += rf"T_{{{i+1}}} &= ({p_val[0]}, {p_val[1]}) \\"
-            podatci_latex += r"\end{aligned}"
-            st.latex(podatci_latex)
+                p_lat += rf"T_{{{i+1}}} &= ({p_val[0]}, {p_val[1]}) \\"
+            p_lat += r"\end{aligned}"
+            st.latex(p_lat)
 
-            # Grupiramo h_k i d_k radi boljeg proreda
-            hk_dk_latex = r"\textbf{Koeficijenti } h_k \text{ i } d_k:\\" + r"\begin{aligned}"
+            # Koeficijenti h
+            h_lat = r"\textbf{Koeficijenti } h_k:\\" + r"\begin{aligned}"
             for i, h_val in enumerate(lh):
-                hk_dk_latex += rf"h_{{{i+1}}} &= {h_val} & d_{{{i+1}}} &= {ld[i]} \\"
-            hk_dk_latex += r"\end{aligned}"
-            st.latex(hk_dk_latex)
-            
+                h_lat += rf"h_{{{i+1}}} &= {h_val} \\"
+            h_lat += r"\end{aligned}"
+            st.latex(h_lat)
+
+            # Koeficijenti d
+            d_lat = r"\textbf{Koeficijenti } d_k:\\" + r"\begin{aligned}"
+            for i, d_val in enumerate(ld):
+                d_lat += rf"d_{{{i+1}}} &= {d_val} \\"
+            d_lat += r"\end{aligned}"
+            st.latex(d_lat)
             st.markdown('</div>', unsafe_allow_html=True)
 
         with c2:
             st.markdown('<div style="text-align: left;">', unsafe_allow_html=True)
             
-            # Grupiramo s_k i b_k
-            sk_bk_latex = r"\textbf{Koeficijenti } s_k \text{ i } b_k:\\" + r"\begin{aligned}"
-            # Koristimo zip ili range da ih prikažemo paralelno radi uštede prostora
-            for i in range(len(ls)):
-                linija = rf"s_{{{i+1}}} &= {ls[i]}"
-                if i < len(lb): # b_k ima jedan manje od s_k obično
-                    linija += rf" & b_{{{i+1}}} &= {lb[i]}"
-                sk_bk_latex += linija + r"\\"
-            sk_bk_latex += r"\end{aligned}"
-            st.latex(sk_bk_latex)
+            # Koeficijenti s
+            s_lat = r"\textbf{Koeficijenti } s_k:\\" + r"\begin{aligned}"
+            for i, s_val in enumerate(ls):
+                s_lat += rf"s_{{{i+1}}} &= {s_val} \\"
+            s_lat += r"\end{aligned}"
+            st.latex(s_lat)
             
+            # Koeficijenti b
+            b_lat = r"\textbf{Koeficijenti } b_k:\\" + r"\begin{aligned}"
+            for i, b_val in enumerate(lb):
+                b_lat += rf"b_{{{i+1}}} &= {b_val} \\"
+            b_lat += r"\end{aligned}"
+            st.latex(b_lat)
             st.markdown('</div>', unsafe_allow_html=True)
 
-        # Matrica i vektor (ostavljamo st.dataframe jer je preglednije za velike sustave)
-            st.divider()
-            col_m1, col_m2 = st.columns([2, 1])
-            with col_m1:
-                st.latex(r"\textbf{Matrica } H")
-                st.dataframe(H_mat, use_container_width=True)
-            with col_m2:
-                st.latex(r"\textbf{Vektor } r")
-                st.dataframe(r_vec, use_container_width=True)
+        # Matrica H u LaTeXu
+        st.markdown('<div style="text-align: left;">', unsafe_allow_html=True)
+        
+        # Generiranje LaTeX koda za matricu
+        h_matrix_latex = r"\textbf{Matrica } H = \begin{bmatrix} "
+        for row in H_mat:
+            h_matrix_latex += " & ".join(map(str, row)) + r" \\ "
+        h_matrix_latex += r"\end{bmatrix}"
+        st.latex(h_matrix_latex)
+
+        # Vektor r u LaTeXu
+        r_vector_latex = r"\textbf{Vektor } r = \begin{bmatrix} "
+        r_vector_latex += r" \\ ".join(map(str, r_vec))
+        r_vector_latex += r" \end{bmatrix}^T" # Transponirano da ne zauzima previše mjesta vertikalno
+        st.latex(r_vector_latex)
+        
+        st.markdown('</div>', unsafe_allow_html=True)
 
 
     except Exception as e:
